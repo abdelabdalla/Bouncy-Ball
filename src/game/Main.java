c0be62bfd8c00b62dcd96469acd6694a3fc36dfe
Yousuf <youmed.tech@gmail.com>
2015-11-16 09:48:27 +0000

    Improvement to the readability of the code y editing several variable names as well as fixing a bug.

--- src/game/Main.java
+++ src/game/Main.java
@@ -36,116 +36,119 @@ public class Main extends JPanel implements ActionListener, KeyListener {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	int score = 0;
-	JLabel points = new JLabel();
-	Timer t = new Timer(10, this);
-	Random r = new Random();
 
-	double tvel = 9 * 2;
-	double g = 0.17 * 2;
-	double speed = 0;
-	double y = 0;
-	double x = 615;
-	int pipe1 = 1280;
-	int pipestep = 300;
-	int[] pipex = { pipe1, pipe1 + pipestep, pipe1 + (2 * pipestep), pipe1 + (3 * pipestep), pipe1 + (4 * pipestep) };
-	int[] pipey = new int[10];
-	int gap = 635;
-	int sped = -2;
+	JLabel scoreLabel = new JLabel();
+	Timer timer = new Timer(10, this);
+	Random rand = new Random();
+
+	int score = 0;
+	int yPos = 0;
+	int xPos = 615;
+	int pipe1X = 1280;
+	int distPipes = 300;
+	int[] pipeX = { pipe1X, pipe1X + distPipes, pipe1X + (2 * distPipes),
+			pipe1X + (3 * distPipes), pipe1X + (4 * distPipes) };
+	int[] pipeY = new int[10];
+	int pipeGap = 635;
+	int pipeSpeed = -2;
 	int clear = 1305;
-	int lin = 640;
-	int hscore;
-	double cloudX = 20;
-	double cloudY = r.nextInt(100);
+	int clearIntersectPoint = 640;
+	int highScore;
+	int cloudX = 20;
+	int cloudY = rand.nextInt(100);
+
+	double terminalVelocity = 18.0;
+	double gravity = 0.34;
+	double yVelocity = 0.0;
 	double cloudSpeed = 0.3;
-	boolean gOver = false;
-	boolean next = false;
-	boolean plus = false;
-	boolean start = false;
-	boolean pause = false;
-	File file = new File("loop1.wav");
+
+	boolean gameOver = false;
+	boolean gameStarted = false;
+	boolean gamePaused = false;
+
+	File soundFile = new File("loop1.wav");
 
 	String playerName;
 
 	public Main(String name) {
+		playerName = name;
 
 		for (int i = 0; i < 10; i += 2) { // initalise y axis for pipes
-			pipey[i] = r.nextInt(600) - 600;
-			pipey[i + 1] = pipey[i] + gap;
+			pipeY[i] = rand.nextInt(600) - 600;
+			pipeY[i + 1] = pipeY[i] + pipeGap;
 		}
 
-		playerName = name;
-		addKeyListener(this);
-		setFocusable(true);
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("HScore.txt"));
-			hscore = br.read();
-			System.out.println(hscore);
+			highScore = br.read();
+			System.out.println(highScore);
 			br.close();
 
 		} catch (Exception e) {
-			hscore = 0;
+			highScore = 0;
 		}
 
-		points.setBounds(20, 20, 100, 50);
-		points.setForeground(Color.RED);
-		points.setFont(new Font("Arial", Font.PLAIN, 30));
-		add(points);
+		scoreLabel.setBounds(20, 20, 100, 50);
+		scoreLabel.setForeground(Color.RED);
+		scoreLabel.setFont(new Font("Arial", Font.PLAIN, 30));
+
+		addKeyListener(this);
+		setFocusable(true);
+		add(scoreLabel);
 
 		// music();
 	}
 
 	public void paintComponent(final Graphics g) {
+		Rectangle[] pipe = new Rectangle[10];
+		Rectangle o1 = new Rectangle((int) xPos, (int) yPos, 35, 35);
 
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+				RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		super.paintComponent(g2d);
 
 		for (int i = 0; i < 10; i += 2) { // initialise pipe y axis
-			while (pipey[i] < -500) {
-				pipey[i] = r.nextInt(600) - 600;
-				pipey[i + 1] = pipey[i] + gap;
+			while (pipeY[i] < -500) {
+				pipeY[i] = rand.nextInt(600) - 600;
+				pipeY[i + 1] = pipeY[i] + pipeGap;
 			}
 		}
 
-		Rectangle[] pipe = new Rectangle[10];
-		for (int i = 0; i < 10; i +=2) { // initialise pipes
-				pipe[i] = new Rectangle(pipex[i/2], pipey[i], 50, 500);
-				pipe[i + 1] = new Rectangle(pipex[i/2], pipey[i + 1], 50, 1000);
+		for (int i = 0; i < 10; i += 2) { // initialise pipes
+			pipe[i] = new Rectangle(pipeX[i / 2], pipeY[i], 50, 500);
+			pipe[i + 1] = new Rectangle(pipeX[i / 2], pipeY[i + 1], 50, 1000);
 		}
-
-		Rectangle o1 = new Rectangle((int) x, (int) y, 35, 35);
 
 		g.setColor(new Color(48, 179, 255));
 		g.fillRect(0, 0, 1280, 720);
-		
+
 		g.setColor(Color.WHITE);
 		for (int i = 300; i <= 1200; i += 300) { // draw clouds
 			paintOvals(g, cloudX, cloudY);
 			paintOvals(g, cloudX + i, cloudY + 30);
 		}
-		
+
 		g.setColor(new Color(0, 212, 49));
 		for (Rectangle p : pipe) { // draw pipes
 			g.fillRect(p.x, p.y, p.width, p.height);
 		}
-		 
 
 		g.setColor(new Color(48, 179, 255));
-		if (start == true) {
+		if (gameStarted == true) {
 			g.setColor(Color.YELLOW);
 		}
 		g.fillRect(o1.x, o1.y, o1.width, o1.height);
 
-		if (start == false && hscore != 0) { // at start
+		if (gameStarted == false && highScore != 0) { // at start
 			g.setColor(Color.RED);
 			g.setFont(new Font("Arial ", Font.PLAIN, 45));
 			g.drawString("Bouncy Box", 510, 100);
 			g.drawString("Press space to begin", 420, 300);
 
-		}
-		else if (start == false && hscore == 0) {
+		} else if (gameStarted == false && highScore == 0) {
 			g.setColor(Color.RED);
 			g.setFont(new Font("Arial", Font.PLAIN, 45));
 			g.drawString("Bouncy Box", 510, 100);
@@ -154,14 +157,24 @@ public class Main extends JPanel implements ActionListener, KeyListener {
 			g.drawString("Don't touch the ground", 385, 350);
 			g.drawString("Press space to begin", 420, 400);
 		}
-		if (o1.intersects(pipe[0]) || o1.intersects(pipe[1]) || o1.intersects(pipe[2]) || o1.intersects(pipe[3]) // hit detection
-				|| o1.intersects(pipe[4]) || o1.intersects(pipe[5]) || o1.intersects(pipe[6]) || o1.intersects(pipe[7])
+		if (o1.intersects(pipe[0])
+				|| o1.intersects(pipe[1])
+				|| o1.intersects(pipe[2])
+				|| o1.intersects(pipe[3]) // hit detection
+				|| o1.intersects(pipe[4]) || o1.intersects(pipe[5])
+				|| o1.intersects(pipe[6]) || o1.intersects(pipe[7])
 				|| o1.intersects(pipe[8]) || o1.intersects(pipe[9])) {
-			t.stop();
-			gOver = true;
+			timer.stop();
+			gameOver = true;
 		}
 
-		if (gOver == true) { //at game end
+		if (gamePaused) { // when paused
+			g.setColor(Color.RED);
+			g.setFont(new Font("Arial ", Font.PLAIN, 45));
+			g.drawString("Paused", 545, 250);
+		}
+
+		if (gameOver) { // at game end
 
 			final int s1 = score;
 			ParseObject object = new ParseObject("Scores");
@@ -169,7 +182,14 @@ public class Main extends JPanel implements ActionListener, KeyListener {
 			object.put("Score", s1);
 			object.saveInBackground(); // save score
 
-			ParseQuery<ParseObject> q = ParseQuery.getQuery("Scores"); //tests to see if score is high score
+			ParseQuery<ParseObject> q = ParseQuery.getQuery("Scores"); // tests
+																		// to
+																		// see
+																		// if
+																		// score
+																		// is
+																		// high
+																		// score
 			q.getInBackground("PseztczTNY", new GetCallback<ParseObject>() {
 
 				@Override
@@ -185,37 +205,40 @@ public class Main extends JPanel implements ActionListener, KeyListener {
 								g.drawString("You're the best", 400, 300);
 
 							} catch (ParseException e1) {
-								// TODO Auto-generated catch block
+
 								e1.printStackTrace();
 							}
 						} else {
-							System.out.println("Highscore: " + ob.getInt("Score") + " by " + ob.getString("User"));
+							System.out.println("Highscore: "
+									+ ob.getInt("Score") + " by "
+									+ ob.getString("User"));
 						}
 					}
 				}
 			});
 
-			
-			  if (hscore < score) { hscore = score;
-			  
-			  try {
-			  
-			  Writer wr = new FileWriter("HScore.txt"); wr.write(hscore);
-			  wr.close();
-			  
-			  } catch (IOException e) { e.printStackTrace(); }
-			  
-			  }
-			 
+			if (highScore < score) {
+				highScore = score;
+
+				try {
+
+					Writer wr = new FileWriter("HScore.txt");
+					wr.write(highScore);
+					wr.close();
+
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+
 			g.setColor(Color.RED);
 			g.setFont(new Font("Arial ", Font.PLAIN, 45));
 			g.drawString("You lose!", 545, 250);
-			g.drawString("Local High Score: " + Integer.toString(hscore), 500, 300);
+			g.drawString("Local High Score: " + Integer.toString(highScore),
+					500, 300);
 			g.drawString("Press space to retry or esc to exit", 295, 350);
 			score = 0;
-
-		}
-		if (pause == true) {
 
 		}
 
@@ -229,46 +252,41 @@ public class Main extends JPanel implements ActionListener, KeyListener {
 
 	public void actionPerformed(ActionEvent e) {
 
-		speed = speed + g;
+		yVelocity += gravity;
 
-		if (speed >= tvel) {
-			speed = tvel;
+		if (yVelocity >= terminalVelocity) {
+			yVelocity = terminalVelocity;
 		}
-		if (y >= 670) {
-			y = 640;
-			gOver = true;
-			t.stop();
+		if (yPos >= 670 || yPos <= -45) {
+			gameOver = true;
+			timer.stop();
 		}
-		if (y <= -45) {
-			gOver = true;
-			t.stop();
-		}
-		y = y + speed;
+		yPos += yVelocity;
 
 		if (clear <= 640) {
-			score = score + 100;
-			points.setText(Integer.toString(score));
-			clear = clear + 300;
+			score += 1;
+			scoreLabel.setText(Integer.toString(score));
+			clear += 300;
 		}
 
 		// Pipe things moving etc.
-		pipex[0] = pipex[0] + sped;
-		pipex[1] = pipex[1] + sped;
-		pipex[2] = pipex[2] + sped;
-		pipex[3] = pipex[3] + sped;
-		pipex[4] = pipex[4] + sped;
-		clear = clear + sped;
-		cloudX = cloudX - cloudSpeed;
-		
-		for(int i = 0; i < 5; i++){
-			if(pipex[i] <= -50){
-				if(i != 0){
-					pipex[i] = pipex[i-1] + 300;
+		pipeX[0] = pipeX[0] + pipeSpeed;
+		pipeX[1] = pipeX[1] + pipeSpeed;
+		pipeX[2] = pipeX[2] + pipeSpeed;
+		pipeX[3] = pipeX[3] + pipeSpeed;
+		pipeX[4] = pipeX[4] + pipeSpeed;
+		clear += pipeSpeed;
+		cloudX -= cloudSpeed;
+
+		for (int i = 0; i < 5; i++) {
+			if (pipeX[i] <= -50) {
+				if (i != 0) {
+					pipeX[i] = pipeX[i - 1] + 300;
 				} else {
-					pipex[0] = pipex[4] + 300;
+					pipeX[0] = pipeX[4] + 300;
 				}
-				pipey[2*i] = r.nextInt(600) - 600;
-				pipey[2*i + 1] = pipey[2*i] + gap;
+				pipeY[2 * i] = rand.nextInt(600) - 600;
+				pipeY[2 * i + 1] = pipeY[2 * i] + pipeGap;
 			}
 		}
 
@@ -282,7 +300,8 @@ public class Main extends JPanel implements ActionListener, KeyListener {
 			@Override
 			public void run() {
 				try {
-					AudioInputStream is = AudioSystem.getAudioInputStream(file);
+					AudioInputStream is = AudioSystem
+							.getAudioInputStream(soundFile);
 					Clip clip = AudioSystem.getClip();
 					clip.open(is);
 					clip.loop(Clip.LOOP_CONTINUOUSLY);
@@ -298,58 +317,66 @@ public class Main extends JPanel implements ActionListener, KeyListener {
 	public void keyPressed(KeyEvent e) {
 		int c = e.getKeyCode();
 		if (c == KeyEvent.VK_SPACE) {
-			speed = -8;
+			yVelocity = -8;
 
-			if (y <= 100) {
-				speed = -4;
+			if (yPos <= 100) {
+				yVelocity = -4;
 			}
 		}
 		if (c == KeyEvent.VK_ESCAPE) {
-			t.stop();
-			pause = true;
+			timer.stop();
+			gamePaused = true;
 		}
-		if (c == KeyEvent.VK_SPACE) {
-			t.start();
-			start = true;
+		if (gamePaused) {
+			if (c == KeyEvent.VK_ENTER) {
+				timer.start();
+				gamePaused = false;
+			}
+		}
 
-			points.setText(Integer.toString(score));
+		if (c == KeyEvent.VK_SPACE && gameStarted == false) {
+			timer.start();
+			gameStarted = true;
+
+			scoreLabel.setText(Integer.toString(score));
 		}
-		if (gOver == true) {
+		if (gameOver) {
+			
 
 			if (c == KeyEvent.VK_SPACE) {
-				t.start();
-
-				gOver = false;
+				timer.start();
+				
 				score = 0;
-				speed = 0;
-				y = 0;
-				pipex[0] = 1280;
-				pipex[1] = pipex[0] + 300;
-				pipex[2] = pipex[1] + 300;
-				pipex[3] = pipex[2] + 300;
-				pipex[4] = pipex[3] + 300;
-				pipey[0] = r.nextInt(600) - 600;
-				pipey[2] = r.nextInt(600) - 600;
-				pipey[4] = r.nextInt(600) - 600;
-				pipey[6] = r.nextInt(600) - 600;
-				pipey[8] = r.nextInt(600) - 600;
-				pipey[1] = pipey[0] + gap;
-				pipey[3] = pipey[2] + gap;
-				pipey[5] = pipey[4] + gap;
-				pipey[7] = pipey[6] + gap;
-				pipey[9] = pipey[8] + gap;
+				scoreLabel.setText(Integer.toString(score));
+				gameOver = false;
+				yVelocity = 0;
+				yPos = 0;
+				pipeX[0] = 1280;
+				pipeX[1] = pipeX[0] + 300;
+				pipeX[2] = pipeX[1] + 300;
+				pipeX[3] = pipeX[2] + 300;
+				pipeX[4] = pipeX[3] + 300;
+				pipeY[0] = rand.nextInt(600) - 600;
+				pipeY[2] = rand.nextInt(600) - 600;
+				pipeY[4] = rand.nextInt(600) - 600;
+				pipeY[6] = rand.nextInt(600) - 600;
+				pipeY[8] = rand.nextInt(600) - 600;
+				pipeY[1] = pipeY[0] + pipeGap;
+				pipeY[3] = pipeY[2] + pipeGap;
+				pipeY[5] = pipeY[4] + pipeGap;
+				pipeY[7] = pipeY[6] + pipeGap;
+				pipeY[9] = pipeY[8] + pipeGap;
 				clear = 1305;
-				lin = 640;
+				clearIntersectPoint = 640;
 			}
 			if (c == KeyEvent.VK_ESCAPE) {
 				System.exit(0);
 			}
 		}
-
 	}
 
 	@Override
-	public void keyReleased(KeyEvent arg0) {
+	public void keyReleased(KeyEvent e) {
 
 	}
 
